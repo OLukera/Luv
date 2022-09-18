4ce8820eda60c7bad5b5436e91ebb97ce7e290ad
Lucas Goncalves <senogamex@gmail.com>
2022-09-17 21:05:13 -0300

    commit inicial

---
+++ cantadas.js
@@ -0,0 +1,194 @@
+//CANTADAS
+var cantadas = [
+    'Tem alguma coisa errada com o meu celular. Não consigo encontrar o seu número nele...',
+    'Você acredita em amor à primeira vista ou devo passar por aqui mais uma vez?',
+    'Então, além de me deixar sem ar, o que mais você faz?',
+    'Queria desejar "noite", porque para ser boa teríamos que estar juntos.',
+    'Se nada dura para sempre, quer ser o meu nada?',
+    'O que eu sinto por você só pode ser motorista, porque passageiro(a) não é.',
+    'Oi tudo bem? Estou meio perdido aqui e queria saber o caminho para o seu coração...',
+    'Acho que to ficando cego, me ajuda aqui... Porque eu não vejo a hora de te beijar..',
+    'Vira seu celular de ponta cabeça!! Agora aproveita e vira o amor da minha vida também.',
+    'Sabe o que você vai fazer daqui um ano? Um ano de namoro comigo!',
+    'Ei Alice, vamo ali Alice? Ali..ce beijar...',
+    'Posso tirar uma foto sua? É para mostrar ao Papai Noel o que eu quero de presente.',
+    'O seu nome é Google? Porque você tem tudo o que eu procuro.',
+    'Desculpa, você estava falando comigo? Não? Então, por favor, comece a falar :)',
+    'Acho que você está com falta de vitamina Eu.',
+    'Ei, o cupido te ligou? Ele queria pedir para você devolver o meu coração.',
+    'Pronto, já estou aqui. Quais são seus outros 2 desejos?',
+    'Do que adianta estudar Física, se você não respeita a lei da nossa atração?',
+    'Pensava que felicidade começava com F, mas começa com você.',
+    'Dizem que a Disneylândia é o lugar mais feliz do mundo. Parece que ninguém sabe como é estar com você.',
+    'Sabe o que combina com você? Eu.',
+    'Sabe estacionar? Porque tem uma vaga no meu coração.',
+    'Ei, você deixou cair uma coisa. “O quê?”. Meu queixo!',
+    'Você gosta de açaí? Então está disposta a açaí comigo',
+  ];
+
+//QUEBRA-GELO
+const quebraGelo = [
+  'Temos tanto em comum. Eu tenho Tinder, você também. Eu moro no Brasil, você também. Acho que nascemos um para o outro (ironia)',
+  'Se você acha que vai me conquistar só por que tem um rosto perfeito e foto na *Europa*… você está completamente certa',
+  'Pode me dizer o que não procura no Tinder? Porque o que procura, acho que é obvio né…. Eu',
+  'Preciso admitir, a descrição no seu perfil me fez sentir que seria um erro não te chamar pra conversar!',
+  'Uma pessoa que escuta a música X, definitivamente é alguém com quem eu quero conversar! (Óbvio, use a música que ele colocou no perfil, se você também gostar dela)',
+  'Vou aproveitar o match para dizer que você fica muito bem de vermelho. (Óbvio, uma cor que ele esteja usando nas fotos)',
+  'A sua terceira foto me deixou curioso(a)…',
+  'Olhei o seu perfil, e vi uma coisa interessante sobre você.',
+  'Algo me diz que temos muitas coisas em comum. Vamos conversar para saber?',
+  'Então quer dizer que você é desse tipo né… (ELA: Que tipo?). Do tipo que fica bem em todas as fotos.',
+  'Meu você gosta de cachorros, torce pro time X, se falar que sabe fazer bolinho de chuva eu vou ter que te pagar um lanche/cerveja no nosso terceiro encontro',
+  'Parece que você sabe como se divertir hahah Teve alguma aventura ultimamente?',
+  'Eu já esperava que combinássemos 😆 Parece que você gosta de muitas coisas que eu faço. (Se você fizer algo do perfil da pessoa)',
+  'Eu acho que conheço alguém que pode ter você como crush! .... Eu',
+]
+
+//QUESTIONADORES
+const questionadores = [
+    'Qual comida você não consegue resistir por nada?',
+    'Qual é o seu lugar preferido em todo o mundo?',
+    'Qual o gênero de música que está sempre nas suas playlists?',
+    'Você acredita em amor à primeira vista?',
+    'Qual seria a viagem dos seus sonhos?',
+    'Qual a última série que você maratonou?',
+    'O que você mais tem orgulho sobre você?',
+    'O que você não perdoaria em um relacionamento?',
+    'O que mais te atrai em uma pessoa?',
+    'O que você mais espera de alguém em um relacionamento?',
+    'Você se considera uma pessoa romântica?',
+    'Como você descreveria o relacionamento perfeito?',
+    'Um dia eu experimentei [tal coisa] e foi um dos pratos mais diferentes que já vi! Você já comeu?',
+    'Qual bebida você mais curte beber?',
+    'quais são os seus hobbies que você mais ama fazer?',
+    'Qual a sua foto favorita no meu Instagram? Por que?',
+    'Qual a sua foto favorita no seu Instagram?',
+    'Tem algum vídeo na internet que sempre te faz rir?',
+    'Sabe tocar algum instrumento musical?',
+]
+
+//QUESTIONADORES
+const criativos = [
+  'Qual comida você não consegue resistir por nada?',
+  'Qual é o seu lugar preferido em todo o mundo?',
+  'Qual o gênero de música que está sempre nas suas playlists?',
+  'Você acredita em amor à primeira vista?',
+  'Qual seria a viagem dos seus sonhos?',
+  'Qual a última série que você maratonou?',
+  'O que você mais tem orgulho sobre você?',
+  'O que você não perdoaria em um relacionamento?',
+  'O que mais te atrai em uma pessoa?',
+  'O que você mais espera de alguém em um relacionamento?',
+  'Você se considera uma pessoa romântica?',
+  'Como você descreveria o relacionamento perfeito?',
+  'Um dia eu experimentei [tal coisa] e foi um dos pratos mais diferentes que já vi! Você já comeu?',
+  'Qual bebida você mais curte beber?',
+  'quais são os seus hobbies que você mais ama fazer?',
+  'Qual a sua foto favorita no meu Instagram? Por que?',
+  'Qual a sua foto favorita no seu Instagram?',
+  'Tem algum vídeo na internet que sempre te faz rir?',
+  'Sabe tocar algum instrumento musical?',
+]
+
+//Emocionais
+const emocionais = [
+    'Qual momento incrível da sua vida você reviveria?',
+    'Qual sonho tem certeza de que ainda vai realizar?',
+    'Qual foi o melhor conselho que você já recebeu?',
+    'Qual foi o dia mais feliz da sua vida? O que aconteceu?',
+    'Quem conhece você melhor que ninguém?',
+    'Você tem planos para o futuro?',
+    'Qual lugar você me levaria quando começarmos a namorar? hahah',
+    'Qual é o lugar mais lindo que você já esteve?',
+    'Quais são suas maiores qualidades?',
+    'O que a maioria das pessoas não sabe sobre você?',
+    'Quais são seus sonhos e objetivos?',
+    'Se pudesse morar em outra cidade, onde seria?',
+    'Qual é a palavra que melhor te define?',
+    'O que você mudaria no mundo?',
+    'Que comida te faz lembrar da infância?',
+    'O que são almas gêmeas para você?',
+    'O que significa ser feliz para você?',
+    'Qual foi e como foi o momento de maior aventura da sua vida até hoje?',
+    'Qual a coisa mais vergonhosa que você fez bêbada?',
+]
+
+//Emocionais
+const sedutores = [
+    'Gosta de massagens?',
+    'O que mais te atrai em uma pessoa?',
+    'Eu troco esse boa noite por uma noite boa na sua companhia hein? Só depende de ti',
+    'Tu gosta de cantar no chuveiro? Me leva pra ver um show qualquer dia desses',
+    'Tô me esforçando muito pra não te dar um beijo agora',
+    'Você é uma gracinha, sabia?',
+    'O que você faria se eu te roubasse um beijo agora?',
+    'Me explica aí porque eu não consigo parar de olhar pro seus olhos e sua boca?',
+    'Nossa que calor… acho que é você do meu lado, hein?',
+    'Eu queria tá fazendo o que a gente não devia tá fazendo ',
+    'Se eu deixasse você fazer qualquer coisa comigo o que você faria primeiro?',
+    'Se você continuar me olhando assim, eu não vou ser responsável pelos meus atos viu?',
+    'Você não sabe o tempo que eu perdi hoje, só pensando nas coisas que a gente pode fazer sábado',
+    'Eu tô morrendo de vontade de te beijar,  e não é só na boca',
+    'Fala um pouquinho mais alto/perto não consigo te ouvir com esse tanto de roupa que a gente tá vestindo',
+    'Você não é muito boa em fingir que não me quer',
+    'Tô te colocando na minha listinha de presentes pro papai Noel',
+    'Prefere beijo na boca ou no pescoço?',
+    'Consegue adivinhar o que eu estou vestindo agora?',
+    'Você prefere transar com alguém lindo ou com alguém que é muito bom de cama?',
+    'Que som deixa você excitada(o)?',
+    'Que sabor você acha que o meu beijo tem?',
+    'O que você faria se eu estivesse na sua cama agora?',
+    'Se você só pudesse me tocar em uma parte, qual seria?',
+    'Se você só pudesse transar na mesma posição pro resto da vida, qual seria?',
+    'O que te faz sentir frio na barriga?',
+    'Tem tatuagem ou piercing escondida? Onde?',
+]
+
+//função cantadas
+function Cantada(){
+
+   var elemento = Math.floor(Math.random() * cantadas.length);  
+
+   document.getElementById("texto").innerHTML = cantadas[elemento];
+}
+
+//função Quebra-Gelo
+function Quebra(){
+
+  var elemento = Math.floor(Math.random() * quebraGelo.length);  
+
+  document.getElementById("texto").innerHTML = quebraGelo[elemento];
+}
+
+//função Questionadores
+function Questionador(){
+
+  var elemento = Math.floor(Math.random() * questionadores.length);  
+
+  document.getElementById("texto").innerHTML = questionadores[elemento];
+}
+
+//função Criativos
+function Criativo(){
+
+  var elemento = Math.floor(Math.random() * criativos.length);  
+
+  document.getElementById("texto").innerHTML = criativos[elemento];
+}
+
+//função Emocionais
+function Emocional(){
+
+  var elemento = Math.floor(Math.random() * emocionais.length);  
+
+  document.getElementById("texto").innerHTML = emocionais[elemento];
+}
+
+//função Sedutores
+function Sedutor(){
+
+  var elemento = Math.floor(Math.random() * sedutores.length);  
+
+  document.getElementById("texto").innerHTML = sedutores[elemento];
+}
+
